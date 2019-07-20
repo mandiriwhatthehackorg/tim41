742d4160127bec9e84276c627a6b5030afac482d
Maulana Akmal <maulana2904@gmail.com>
2019-07-20 13:25:13 +0700

    Initial Commit

---
+++ Pinjamify/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pinjamify
+//
+//  Created by Maulana Akmal  on 20/07/19.
+//  Copyright © 2019 Maulana Akmal . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
